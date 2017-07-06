9d03f43e83bce8b03f6d82c2d2383395f4a1ba20
Aakash Garg <aakash.garg80@gmail.com>
2017-07-06 06:10:10 +0000

    search db and word count of query

--- public/search.php
+++ public/search.php
@@ -6,7 +6,16 @@
     $places = [];
 
     // TODO: search database for places matching $_GET["geo"], store in $places
-
+        $geo=$_GET["geo"];
+        $geo=str_replace(",", " ", $geo);
+        $geo = trim($geo);
+        
+        //word count entered into search..
+        $geo = explode(" ", $geo);
+        $word_count = count($geo);
+        if($word_count< 1){
+            print("Enter query..");
+        }
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
     print(json_encode($places, JSON_PRETTY_PRINT));
